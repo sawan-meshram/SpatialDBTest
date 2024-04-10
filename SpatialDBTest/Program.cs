7e69010bef74372067a59747cd1fc11b33010c88
Sawan Meshram <sawan.shatam@gmail.com>
2024-04-10 16:31:34 +0530

    first commit

---
+++ SpatialDBTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SpatialDBTest
+{
+    class MainClass
+    {
+
+        public static void Main(string[] args)
+        {
+            SpatialDBTest.Test();
+        }
+
+    }
+}
